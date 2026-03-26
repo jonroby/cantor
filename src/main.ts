778b0a91846e70e430e663599fed400567de25d4
jonroby <35011179+jonroby@users.noreply.github.com>
2026-03-26 18:14:21 -0500

    remove outdated kit stuff

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { mount } from 'svelte';
 import 'katex/dist/katex.min.css';
 import './index.css';
-import './routes/layout.css';
+import './layout.css';
 import App from './App.svelte';
 
 const app = mount(App, {
