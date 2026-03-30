7cb9bff1a59eb91bff6a4256f9baf20117655a9a
jonroby <35011179+jonroby@users.noreply.github.com>
2026-03-30 17:10:02 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [tailwindcss(), svelte(), devtoolsJson()],
 	resolve: {
 		alias: {
-			'@': '/Users/jonroby/ai/cantor/src'
+			'@': path.resolve(__dirname, 'src')
 		},
 		conditions: process.env.VITEST ? ['browser'] : undefined
 	},
