ec2572f78ed905d44bae5537232a780e1873e59e
jonroby <35011179+jonroby@users.noreply.github.com>
2026-04-05 09:38:59 -0500

    path

--- src/view/components/landing/AppScreenRecording.svelte
+++ src/view/components/landing/AppScreenRecording.svelte
@@ -1,6 +1,5 @@
-<svg viewBox="0 0 1280 720" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:block;width:100%;height:auto;">
-  <image href="/src/view/components/landing/landing-svg/background.svg" x="0" y="0" width="1280" height="720" />
-  <image href="/src/view/components/landing/landing-svg/sidebar.svg" x="0" y="0" width="1280" height="720" />
-  <image href="/src/view/components/landing/landing-svg/chat-messages.svg" x="0" y="0" width="1280" height="720" />
-  <image href="/src/view/components/landing/landing-svg/composer.svg" x="0" y="0" width="1280" height="720" />
-</svg>
+<script>
+	import WorkflowChat from './WorkflowChat.svelte';
+</script>
+
+<WorkflowChat />
